6a3e0beab9126125ecc1839cb9c894f65da8b0c5
3-Sanins <sanins121421@gmail.com>
2026-01-29 15:47:34 +0530

    Update tournament.js

--- tournament.js
+++ tournament.js
@@ -54,7 +54,7 @@ function loadTournaments() {
 
                     card.onclick = () => {
                         localStorage.setItem("currentTournament", t.key);
-                        window.location.href="tournaments.html?tournamentname="+currentTournament;
+                        window.location.href="tournaments.html?tournamentname="+t.key;
                         // future navigation
                     };
 
