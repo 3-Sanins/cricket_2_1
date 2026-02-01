40e836292d710ee658da81c70b81fbab015d2c57
3-Sanins <sanins121421@gmail.com>
2026-02-01 15:38:28 +0530

    Convert tournament name to uppercase from URL params

--- game_ready.js
+++ game_ready.js
@@ -91,7 +91,7 @@ function init() {
   // To use URL params instead (for production), comment above and uncomment below:
   
   const params = new URLSearchParams(window.location.search);
-  tournamentName = params.get("tournamentname");
+  tournamentName = params.get("tournamentname").toUpperCase();
   matchType = params.get("matchtype") || "league";
   user1 = params.get("user1");
   user2 = params.get("user2");
